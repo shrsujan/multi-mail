4842a35c05b4f1aa1f9cea6830faea6eff2af194
Sujan Shrestha <shrsujan2007@gmail.com>
2016-10-19 21:19:26 +0545

    Update test.js

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ var mailer = require('./index');
 var mailConfig = {
 	service: 'gmail',
 	email: 'youremail@gmail.com',
-    password: 'yourpassword',
-    from: 'YourName <youremail@gmail.com>',
+    	password: 'yourpassword',
+    	from: 'YourName <youremail@gmail.com>',
 	to: ['receiver1@gmail.com', 'receiver2@gmail.com'],
-    subject: 'test test'
+    	subject: 'test test'
 };
 
 var html = "<h1>hello all</h1><br/><p>TESTING MAIL</p>";
